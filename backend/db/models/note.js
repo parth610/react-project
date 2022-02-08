a54c2a512c8cad5987e6a2978c5721cfdb2092ec
Parth Bhakta <bhaktaparthh@gmail.com>
2022-02-07 17:30:16 -0700

    database-creation-done

--- backend/db/models/note.js
+++ backend/db/models/note.js
@@ -1,14 +1,27 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Note = sequelize.define('Note', {
-    title: DataTypes.STRING,
-    content: DataTypes.TEXT,
-    user_id: DataTypes.INTEGER,
-    notebook_id: DataTypes.INTEGER
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    notebook_id:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
   }, {});
   Note.associate = function(models) {
     // associations can be defined here
     Note.belongsTo(models.User, {foreignKey: 'user_id'})
+    Note.belongsTo(models.Notebook, {foreignKey: 'notebook_id'})
   };
   return Note;
 };
